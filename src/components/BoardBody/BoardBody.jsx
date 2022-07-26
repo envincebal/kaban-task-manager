97984ef20dab4b4db3ded5935e5031761c4643fa
envincebal <vincentyan1@gmail.com>
2022-07-26 00:47:03 -0700

    Adding Board Columns component.

--- src/components/BoardBody/BoardBody.jsx
+++ src/components/BoardBody/BoardBody.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import Button from "../Button/Button";
+import BoardColumn from "../BoardColumn/BoardColumn";
 import "./BoardBody.scss";
 
 const BoardBody = () => {
   return (
     <div className="board-body">
-      <div className="empty-board-div">
+      <BoardColumn /> 
+      {/* <div className="empty-board-div">
+        <div className="empty-content">
         <h3>This board is empty. Create a new column to get started.</h3>
         <Button className={"add-task"} text={"+ Add New Column"} />
-      </div>
+        </div>
+
+      </div> */}
     </div>
   )
 }
---
+++ src/components/BoardColumn/BoardColumn.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./BoardColumn.scss";
+
+const BoardColumn = () => {
+  return (
+    <div className="board-column">
+      <h3><span className="board-color"></span>TODO({3})</h3>
+      <ul>
+        
+      </ul>
+    </div>
+  )
+}
+
+export default BoardColumn
