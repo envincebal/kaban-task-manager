b5aee5f5aaa233065ce80adf60e7ca1fc8a61ceb
envincebal <vincentyan1@gmail.com>
2022-08-28 20:37:31 -0700

    Added gh-pages.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
       <div className="logo-div">
         <img className="logo-img" src={theme ?darkLogo : lightLogo} alt="logo"/>
 
-      </div>
+      </div>  
       <div className="main-navbar">
         <div className="mobile-logo-div">
           <svg width="24" height="25" xmlns="http://www.w3.org/2000/svg"><g fill="#635FC7" fillRule="evenodd"><rect width="6" height="25" rx="2"/><rect opacity=".75" x="9" width="6" height="25" rx="2"/><rect opacity=".5" x="18" width="6" height="25" rx="2"/></g></svg>
